af6e87f5ed0580b40c07e5e7884a50d075f6bd0e
Niki <deryuginceresit@yandex.ru>
2021-10-13 11:08:22 +0300

    Subscribe make subscribe is work

--- backend/foodgram/users/serializers.py
+++ backend/foodgram/users/serializers.py
@@ -2,19 +2,32 @@ from rest_framework import serializers
 from .models import User, Subscriptions
 from djoser.serializers import UserSerializer, UserCreateSerializer
 
+
 class UserSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = User
-        fields = ( 'id', 'username', 'email', 'first_name', 'last_name')
+        fields = ('email',
+                  'id',
+                  'username',
+                  'first_name',
+                  'last_name',)
 
 
 
 class SubscriptionsSerializer(serializers.ModelSerializer):
     author = UserSerializer()
-
     class Meta:
         model = Subscriptions
         fields = ('author',)
+
+class SubscribeCreateSerializer(serializers.ModelSerializer):
+    email = serializers.SlugRelatedField(queryset=User.objects.all(),
+                                         slug_field='email')
+    class Meta:
+        model = Subscriptions
+        fields = ('email',)
+
 
 
 
--- backend/foodgram/users/views.py
+++ backend/foodgram/users/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, \
+    ListModelMixin
 from rest_framework.views import APIView
-from .serializers import UserSerializer, SubscriptionsSerializer
+from rest_framework.viewsets import GenericViewSet
+
+from .serializers import UserSerializer, SubscriptionsSerializer, SubscribeCreateSerializer
 from .models import User, Subscriptions
 from rest_framework.response import Response
 from rest_framework.generics import get_object_or_404
 
+class CreateListDestroyViewSet(ListModelMixin,
+                               CreateModelMixin,
+                               DestroyModelMixin,
+                               GenericViewSet):
+    pass
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -22,13 +31,20 @@ class SubscriptionsList(viewsets.ModelViewSet):
 
 
 class SubscriptionsCreate(viewsets.ModelViewSet):
+    http_method_names = ['get', 'delete']
+    queryset = Subscriptions.objects.all()
     serializer_class = SubscriptionsSerializer
-    def get_queryset(self):
+
+    def list(self,request, user_id):
         user = self.request.user
         user_id = self.kwargs.get('user_id')
         author = get_object_or_404(User, id=user_id)
         subscribe = Subscriptions.objects.create(user=user, author=author)
-        return subscribe
+        serializer = UserSerializer(author)
+        return Response(serializer.data)
 
+    def destroy(self, request, user_id):
+        user = self.request.user
+        user_id = self.kwargs.get('user_id')
 
 
