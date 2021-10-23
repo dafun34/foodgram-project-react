6a7904be34c1ffe2b970c2c318ba3c5f52da4e5e
Niki <deryuginceresit@yandex.ru>
2021-10-23 16:19:37 +0300

    Try redact permissions

--- backend/foodgram/recipes/permissions.py
+++ backend/foodgram/recipes/permissions.py
@@ -3,7 +3,10 @@ from rest_framework import permissions
 
 class IsAuthorOrAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.method in permissions.SAFE_METHODS
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        else:
+            return request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
